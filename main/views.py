0a3e99872d9522131c08b26dbcada0847f2abfba
aamirzzzzzz <aamirchawre@gmail.com>
2025-08-29 02:08:00 +0530

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth import logout
+from courses.models import Course 
+
+def home(request):
+    if request.user.is_authenticated:
+        courses = Course.objects.all()
+        return render(request, 'main/dashboard.html', {'courses': courses})
+    else:
+        courses = Course.objects.all()[:3] 
+        return render(request, 'main/home.html', {'courses': courses})
